8852415fc318e9921c08c644c447f1a9985eb63f
Rares <rsahleanu@gmail.com>
2020-02-14 23:14:11 +0100

    init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    using namespace std;
+
+    wstring toEncrypt = L"Test";
+    wchar_t keyToEncrypt = 'd';
+
+    for (int temp = 0; temp < toEncrypt.size(); temp++)
+        toEncrypt[temp] ^= keyToEncrypt;
+    wcout << "The encrypted data = " << toEncrypt;
+
+    for (int temp = 0; temp < toEncrypt.size(); temp++)
+        toEncrypt[temp] ^= keyToEncrypt;
+    wcout << "\nThe unencrypted data = " << toEncrypt;
     return 0;
 }
